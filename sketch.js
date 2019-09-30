57009415f811a62b75712d22ca7a59cdba709010
JayanakaWijethunga <jayanaka987@gmail.com>
2019-09-30 10:06:35 -0700

    finish the knn

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 
+let knn;
 let extractedFeatures;
 let capturedVideo;
-let KNNclassifier;
+let ShowResult;
+let ClassificationReady=false;
 
 
 function setup(){
@@ -14,17 +16,73 @@ function setup(){
 
 	//this featureExtractor which includes in ml5 libaray is used to load mobilenet feature extractor to this project
 
-    extractedFeatures=ml5.featureExtractor('MobileNet',ModelReady);
-    
-    KNNclassifier=ml5.kNNClassifier();
+	extractedFeatures=ml5.featureExtractor('MobileNet',ModelReady);
 
+	//this KNNClassifier() in ml5 helps to works with KNN classification in this project
+
+	knn=ml5.KNNClassifier();
+
+
+	ShowResult=createP("Need Training Data!");
+	ShowResult.style('font-size','32pt');
+}
+
+function StartClassification(){
+
+	//capture the logits of current frame of the video and store in logits constant
+
+	const logist=extractedFeatures.infer(capturedVideo); 
+
+	//classify the current frame
+	knn.classify(logist,function(error,res){
+
+		if(error){
+	
+			console.log(error);
+	
+	
+		}else{
+			ShowResult.html(res.label);
+			
+			StartClassification();
+		}
+	
+	});
 
 }
 
-function mousePressed(){
 
- const getLogits = extractedFeatures.infer(capturedVideo);
- console.log(getLogits.dataSync());
+function keyTyped(){
+
+	const logist=extractedFeatures.infer(capturedVideo);
+
+	//to get the current logits in a tensor form we can use logist.dataSync() 
+
+	if(key == "l"){
+		console.log("Left");
+
+		//add the logits of current frame to the relevent categry store
+
+		knn.addExample(logist,"Left");
+		
+	}else if(key == "r"){
+		console.log("Right");
+		knn.addExample(logist,"Right");
+		
+	}else if(key == "u"){
+		console.log("Up");
+		knn.addExample(logist,"Up");
+		
+	}else if(key == "d"){
+		console.log("Down");
+		knn.addExample(logist,"Down");
+		
+	}
+	else if(key == "s"){
+		console.log("Save");
+		save(knn,"model.json");
+		
+	}		
 
 }
 
@@ -38,7 +96,12 @@ function draw(){
 
 image(capturedVideo,0,0);
 
+//if there is any trained data just start the clasification
+if(!ClassificationReady && knn.getNumLabels()>0){
 
+	StartClassification();
+	ClassificationReady=true;
+}
 
 }
 
